9a0fda466733b72f579284bfdc17aea5a7b46b65
Zuosy <2604208383@qq.com>
2017-11-29 10:08:48 +0800

    2017年11月29日 10:08:45

---
+++ myfirst.cpp
@@ -0,0 +1,20 @@
+//myfirst.cpp -- display a message
+#include<iostream>
+int main()
+{
+	using namespace std;
+	cout << "Come up and C++ me some time.";
+	cout << endl;
+	cout << "You won't regret it!" << endl;
+    return 0;
+}
+/*
+返回类型 函数名(形参列表或形参列表)
+int main()
+{
+	statement;
+	return 0;
+}
+经典的C函数头
+main() 
+*/
--- test.cpp
+++ test.cpp
@@ -1,34 +1,17 @@
 #include<iostream>
+#include<cmath> 
 using namespace std;
+//using std::cout;
+//using std::endl;
 int main(){
-	//cout << "Hello World" << endl;
-	//double ctemp;
-	//cout << "Input a Celsius temp and press ENTER: ";
-	//cin >> ctemp;
-	//cout << "Fahr. temp is: " << (ctemp * 1.8) + 32 << endl;
-//	double n;
-//	cout << "Input a number and press ENTER: ";
-//	cin >> n;
-//	cout << "The square is: " << n * n << endl;
-//	 double ftemp;
-//	 cout << "Input a F temp and press Enter: ";
-//	 cin >> ftemp;
-//	 cout << "C temp is: " << (ftemp - 32) / 1.8 << endl;
-//	 int n;
-//	 cout << "Enter a number: ";
-//	 cin >>n;
-//	 cout << n * n * n << endl;
-
-
-
-
-
-
-
-
-
-
-
+//	cout << "Hello!" << endl;
+//	string str = "Hello World!!!";
+//	cout << str << endl;
+//	cout << "The Good, the";
+//	cout << "Bad, ";
+//	cout << "and the Ukulele";
+//	cout << endl;
+	
 	
 	
 	
